9df7d3022bbf0cac8a71762f517dba10d7b97770
= <=>
2026-03-13 18:26:06 +0100

    base files for host queen

---
+++ hosts/queen/configuration.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+     ../../modules/boot/systemd-boot.nix
+     ../../configuration.nix # Global configuration
+     ./hardware-configuration.nix # Hardware specific configuration
+     ../../modules/fonts.nix
+     ../../users/kurik/kurik.nix
+     ../../modules/desktop/hyprland.nix
+   ];
+}
