5073a8635d1f07c3c77589811484b401fcbb2cf7
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-03-18 08:41:17 +0100

    git.

--- users/kurik/home.nix
+++ users/kurik/home.nix
@@ -32,6 +32,26 @@ in
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
+  programs.gpg.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "François \"Kurikawa\" Odin";
+      email = "francois@kurikawa.fr";
+    };
+    signing = {
+      key = "B82830341F5577C0";
+      signByDefault = true;
+      format = "openpgp";
+    };
+  };
+
   xdg.userDirs = {
     enable = true;
     createDirectories = true; # ONLY if not already created
