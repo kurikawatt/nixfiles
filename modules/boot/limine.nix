a8bfaa72f9771eba4821c25be9161799ebbcbebf
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-03-21 12:21:57 +0100

    only show 5 gens, and removed style (may change in the future)

--- modules/boot/limine.nix
+++ modules/boot/limine.nix
@@ -5,6 +5,19 @@
   boot.loader.limine = {
     enable = true;
     enableEditor = false;
+    maxGenerations = 5;
+  };
+
+  boot.loader.limine.style = {
+    backdrop = "000000";
+    wallpapers = [];
+    interface = {
+      branding = "";
+    };
+    graphicalTerminal = {
+      background = "FF000000";
+      foreground = "FFFFFF";
+    };
   };
 
   boot.loader.efi.canTouchEfiVariables = true;
