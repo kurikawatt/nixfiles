c690cada11aaab986e1d233889b9e70f23a8042b
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-03-30 18:27:39 +0200

    added peers settings to fuuka.nix settings file

--- settings/fuuka.nix
+++ settings/fuuka.nix
@@ -4,14 +4,56 @@
 }:
 let
   inherit (lib) mkOption types;
+
+  peer = {
+    options = {
+      ipv4 = mkOption {
+        type = types.str;
+        default = "172.16.195.0";
+        description = "Peer IPv4 on fuuka";
+      };
+      publicKey = mkOption {
+        type = types.str;
+        default = "";
+        description = "WireGuard's Public Key of peer";
+      };
+    };
+  };
 in
 {
   options.me.services = {
     fuuka = {
+
       enable = mkOption {
         type = types.bool;
         default = true;
         description = "Connect to fuuka (my VPN)";
+      };
+
+      hub = mkOption {
+        type = types.str;
+        default = "chord";
+        description = "Define which peer is the hub";
+      };
+
+    };
+  };
+
+  options.me.services.fuuka.peers = mkOption {
+    description = "List of all Peers on fuuka";
+    type = types.attrsOf (types.submodule peer);
+    default = {
+      chord = {
+        ipv4 = "172.16.195.1";
+        publicKey = "sMoQytVwjqnxSq+MB+7ori5HZr32G866femOjuD17C0=";
+      };
+      aigis = {
+        ipv4 = "172.16.195.12";
+        publicKey = "NoMBDUInInr8FOij+5yb81WkVveSulARGKZSZgy/Rl0=";
+      };
+      queen = {
+        ipv4 = "172.16.195.13";
+        publicKey = "OOkD3dCKw7YOO99cCEQOOWoxwTnYLGZ/eebUG9hpuGk=";
       };
     };
   };
