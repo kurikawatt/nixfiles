f1792bb8e07742438a476cb04b12456abfa53afb
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-04-02 18:49:30 +0200

    age use tpm key, not my ssh key

--- hosts/aigis/configuration.nix
+++ hosts/aigis/configuration.nix
@@ -1,20 +1,19 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }:
 {
   imports = [
-     ../../modules/boot/limine.nix
-     ../../configuration.nix # Global configuration
-     ./hardware-configuration.nix # Hardware specific configuration
-     ../../modules/fonts.nix
-     ../../users/kurik/kurik.nix
-     ../../networks/wifi.nix
-     ../../modules/desktop/hyprland.nix
-     ../laptop-common/battery.nix
-   ];
+    ../../modules/boot/limine.nix
+    ../../configuration.nix # Global configuration
+    ./hardware-configuration.nix # Hardware specific configuration
+    ../../modules/fonts.nix
+    ../../users/kurik/kurik.nix
+    ../../networks/wifi.nix
+    ../../modules/desktop/hyprland.nix
+    ../laptop-common/battery.nix
+  ];
 
   boot.initrd.availableKernelModules = [ "tpm_tis" "tpm_crb" ];
 
@@ -39,8 +38,8 @@
   ];
 
   sops.gnupg.sshKeyPaths = [ ];
-  sops.age.sshKeyPaths = [ 
-    "/home/kurik/.ssh/id_ed25519" 
+  sops.age.sshKeyPaths = [
+    # "/home/kurik/.ssh/id_ed25519" # disabled because I use TPM keys 
   ];
   sops.age = {
     keyFile = "/etc/tpm_age";
