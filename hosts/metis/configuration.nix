16f5323be34f166db010f2b9744a781a48d20cd8
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-04-03 23:04:28 +0200

    putting disko in metis config

--- hosts/metis/configuration.nix
+++ hosts/metis/configuration.nix
@@ -5,12 +5,15 @@
 }:
 {
   imports = [
+    ./disko.nix
     ./hardware-configuration.nix
     ../../modules/boot/systemd-boot.nix
     ../../configuration.nix # Global configuration
     ../../modules/fonts.nix
     ../../users/kurik/kurik.nix
   ];
+
+  disko.devices.disk.main.device = "/dev/sda";
 
   sops.age.sshKeyPaths = [
     "/home/kurik/.ssh/id_ed25519"
