36be9710e6b87e732b014bc92163298e27e27b6d
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-04-05 20:07:39 +0200

    added uid & gid to Monolith mount

--- hosts/metis/configuration.nix
+++ hosts/metis/configuration.nix
@@ -28,7 +28,7 @@
       let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
       in
-      [ "${automount_opts},credentials=/etc/smb-secrets,uid=1001,gid=100" ];
+      [ "${automount_opts},credentials=/etc/smb-secrets,uid=1000,gid=100,dir_mode=0777,file_mode=0777,noperm" ];
   };
 
   sops.age.sshKeyPaths = [
