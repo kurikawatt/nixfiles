5f9c7b9254cc9959af115644bc084624692f9340
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-04-07 17:44:53 +0200

    modfied mount options for Eva02 on metis

--- hosts/metis/configuration.nix
+++ hosts/metis/configuration.nix
@@ -34,7 +34,7 @@
   fileSystems."/media/Eva02" = {
     device = "/dev/disk/by-label/Eva02";
     fsType = "ext4";
-    options = [ "auto" "nofail" "fmask=0777" "dmask=0777" ];
+    options = [ "auto" "nofail" ];
   };
 
   sops.age.sshKeyPaths = [
