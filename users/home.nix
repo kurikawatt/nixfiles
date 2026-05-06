1f0eca758816eb017011f7c45b57c98e118e3768
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-05-06 20:01:37 +0200

    added obs

--- users/home.nix
+++ users/home.nix
@@ -119,6 +119,25 @@ in
     ];
   };
 
+  programs.obs-studio = {
+    enable = true;
+
+    # Nvidia hardware acceleration
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   programs.gpg.enable = true;
 
   services.gpg-agent = {
