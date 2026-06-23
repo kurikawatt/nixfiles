24a9c7137330fe889c6704b9963a29f9fa736faf
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-06-23 10:34:36 +0200

    waybar: added critical threshold for cpu temp

--- scripts/average-cpu-temp.sh
+++ scripts/average-cpu-temp.sh
@@ -5,9 +5,10 @@ CPU_HWMON="/sys/class/hwmon/hwmon2"
 total=0
 count=0
 
-# Loop through all available temperature inputs in your coretemp sensor
+critical_threshold=80
+
 for temp_file in $CPU_HWMON/temp*_input; do
-    # Ensure the file exists to prevent errors
+
     if [ -f "$temp_file" ]; then
         temp=$(cat "$temp_file")
         total=$((total + temp))
@@ -15,12 +16,15 @@ for temp_file in $CPU_HWMON/temp*_input; do
     fi
 done
 
-# Calculate the average and convert from millidegrees to standard Celsius
 if [ $count -gt 0 ]; then
     avg=$((total / count / 1000))
     
-    # Output JSON format so Waybar can easily read it
-    echo "{\"text\": \"$avg\"}"
+    if [ "$avg" -ge "$critical_threshold" ]; then
+        echo "{\"text\": \"$avg\", \"class\": \"critical\"}"
+    else
+        echo "{\"text\": \"$avg\", \"class\": \"normal\"}"
+    fi
+
 else
     echo "{\"text\": \"N/A\"}"
 fi
--- users/waybar/sysmonitors.nix
+++ users/waybar/sysmonitors.nix
@@ -5,31 +5,46 @@
 , ...
 }:
 {
-  programs.waybar.settings.main = {
-  
-    modules-left = [ "group/cpu-temp" "memory" ];
+  programs.waybar = {
     
-    "group/cpu-temp" = {
-      orientation = "horizontal";
-      modules = [ "cpu" "custom/cpu-temp" ];
+    settings.main = {
+
+      modules-left = [ "group/cpu-temp" "memory" ];
+
+      "group/cpu-temp" = {
+        orientation = "horizontal";
+        modules = [ "cpu" "custom/cpu-temp" ];
+      };
+
+      cpu = {
+        interval = 10;
+        format = "cpu : {usage}% ";
+      };
+
+      "custom/cpu-temp" = {
+        exec = "~/scripts/average-cpu-temp.sh";
+        interval = 10;
+        return-type = "json";
+        format = "({}°C)";
+      };
+
+      memory = {
+        interval = 10;
+        format = "mem : {used:0.1f}/{total:0.1f}Go";
+      };
+
     };
 
-    cpu = {
-      interval = 10;
-      format = "cpu : {usage}% ";
-    };
+    style = ''
+      #custom-cpu-temp {
+        color: #e0def4;
+      }
 
-    "custom/cpu-temp" = {
-      exec = "~/scripts/average-cpu-temp.sh";
-      interval = 10;
-      return-type = "json";
-      format = "({}°C)";
-    };
-
-    memory = {
-      interval = 10;
-      format = "mem : {used:0.1f}/{total:0.1f}Go";
-    };
+      #custom-cpu-temp.critical {
+        color: #eb6f92;
+        font-weight: bold;
+      }
+    '';
 
   };
 }
