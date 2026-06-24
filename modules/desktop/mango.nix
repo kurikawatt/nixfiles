da80a6fc070bca5de6cb40c31fcef0dc7897e4b6
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-06-24 21:41:37 +0200

    foot colors now uses the palettes defined in settings.nix

--- modules/desktop/mango.nix
+++ modules/desktop/mango.nix
@@ -27,27 +27,30 @@ lib.mkIf (config.me.desktop == "mango") {
         hide-when-typing = "yes";
       };
       colors-dark = {
-        # Rose Piné
-        background = "191724";
-        foreground = "e0def4";
 
-        regular0 = "26233a";
-        regular1 = "eb6f92";
-        regular2 = "9ccfd8";
-        regular3 = "f6c177";
-        regular4 = "31748f";
-        regular5 = "c4a7e7";
-        regular6 = "ebbcba";
-        regular7 = "e0def4";
+        background = config.me.colors.background;
+        foreground = config.me.colors.foreground;
 
-        bright0 = "47435d";
-        bright1 = "ff98ba";
-        bright2 = "c5f9ff";
-        bright3 = "ffeb9e";
-        bright4 = "5b9ab7";
-        bright5 = "eed0ff";
-        bright6 = "ffe5e3";
-        bright7 = "fefcff";
+        regular0 = builtins.elemAt config.me.colors.regulars 0;
+        regular1 = builtins.elemAt config.me.colors.regulars 1;
+        regular2 = builtins.elemAt config.me.colors.regulars 2;
+        regular3 = builtins.elemAt config.me.colors.regulars 3;
+        regular4 = builtins.elemAt config.me.colors.regulars 4;
+        regular5 = builtins.elemAt config.me.colors.regulars 5;
+        regular6 = builtins.elemAt config.me.colors.regulars 6;
+        regular7 = builtins.elemAt config.me.colors.regulars 7;
+
+        bright0 = builtins.elemAt config.me.colors.brights 0;
+        bright1 = builtins.elemAt config.me.colors.brights 1;
+        bright2 = builtins.elemAt config.me.colors.brights 2;
+        bright3 = builtins.elemAt config.me.colors.brights 3;
+        bright4 = builtins.elemAt config.me.colors.brights 4;
+        bright5 = builtins.elemAt config.me.colors.brights 5;
+        bright6 = builtins.elemAt config.me.colors.brights 6;
+        bright7 = builtins.elemAt config.me.colors.brights 7;
+
+        dim0 = builtins.elemAt config.me.colors.dimmed 0;
+        dim1 = builtins.elemAt config.me.colors.dimmed 1;
 
         flash = "f6c177";
 
