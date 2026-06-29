51a29938c794ff653cdc19deeb998dad6c133a1e
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-06-29 23:55:45 +0200

    changed xdg-portal

--- users/home.nix
+++ users/home.nix
@@ -32,7 +32,7 @@ in
     xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-wlr
     ];
     config.common.default = "*";
   };
