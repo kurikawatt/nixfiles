c5da3b059dbef736926d8f5e1bf687d7497abf41
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-07-03 22:02:27 +0200

    attic server should use localhost instead of metis ipv4 on fuuka

--- modules/core/attic.nix
+++ modules/core/attic.nix
@@ -1,13 +1,17 @@
 {
   pkgs,
+  config,
   ...
 }:
+let
+  inherit (config.me.services) attic-server;
+in
 {
   # Use my attic cache for faster rebuild
   nix.settings = {
-    substituters = [
-      "http://172.16.195.2:8080/compendium"
-    ];
+    substituters = 
+      if attic-server.enable then [ "http://localhost:${toString attic-server.port}/compendium" ]
+      else [ "http://172.16.195.2:8080/compendium" ];
     trusted-public-keys = [
       "compendium:sUVIH8kmLdxpo5pTLnlSaOcR/dNP4dTjDwanQFOKYV4="
     ];
