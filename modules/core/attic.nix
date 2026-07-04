7ae961cc05fc684774910635bb550e5f5d85485d
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-07-04 17:04:52 +0200

    attic watch-store service

--- modules/core/attic.nix
+++ modules/core/attic.nix
@@ -1,17 +1,24 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }:
 let
   inherit (config.me.services) attic-server;
   inherit (config.me.services.fuuka) peers;
+
+  url = "http://${peers.metis.ipv4}:${toString attic-server.port}";
 in
 {
+  sops.secrets."attic-tokens/compendium" = {
+    restartUnits = [ "attic-watch-store.service" ];
+  };
+
   # Use my attic cache for faster rebuild
   nix.settings = {
     substituters = [
-      "http://${peers.metis.ipv4}:${toString attic-server.port}/compendium"
+      "${url}/compendium"
     ];
     trusted-public-keys = [
       "compendium:sUVIH8kmLdxpo5pTLnlSaOcR/dNP4dTjDwanQFOKYV4="
@@ -19,4 +26,23 @@ in
   };
 
   environment.systemPackages = with pkgs; [ attic-client ];
+
+  # Almost identical to ayko's (0x7E on codeberg) service, just
+  # changed server & cache names
+  systemd.services.attic-watch-store = {
+    description = "Attic watch store";
+    wantedBy = [ "multi-user.target" ];
+    environment.ATTIC_SERVER = url;
+    serviceConfig = {
+      ExecStartPre = pkgs.writeShellScript "attic-login" ''
+        TOKEN=$(cat ${config.sops.secrets."attic-tokens/compendium".path})
+        ${lib.getExe pkgs.attic-client} login metis ${url} $TOKEN
+      '';
+      ExecStart = "${lib.getExe pkgs.attic-client} watch-store compendium";
+      Restart = "always";
+      RestartSec = 3;
+      KillMode = "control-group";
+      KillSignal = "SIGTERM";
+    };
+  };
 }
