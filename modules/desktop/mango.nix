847196bb85a7978a6b69c7024d8e13a5326a2dc7
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-09-10 20:33:53 +0200

    updated flake.lock (fuck nvidia)

--- modules/desktop/mango.nix
+++ modules/desktop/mango.nix
@@ -17,7 +17,15 @@ lib.mkIf (config.me.host.desktop == "mango") {
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          chooser_type = "simple";
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        };
+      };
+    };
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
